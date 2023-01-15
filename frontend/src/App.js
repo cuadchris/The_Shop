02205395e199195466b90798c4bf2febc37c5215
Christopher <cuadradochris@hotmail.com>
2023-01-15 13:28:18 -0500

    react-bootstrap. header, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <h1>The Shop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>The Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; The Shop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+export default Footer;

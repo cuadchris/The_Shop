8d1d69d81b5f66e2b06c98426d28fb7dfc5097d1
Christopher <cuadradochris@hotmail.com>
2023-01-16 10:51:33 -0500

    home page. product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>The Shop</h1>
+          <Home/>
         </Container>
       </main>
       <Footer />

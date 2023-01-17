c936dc079fece17c66b9b1ca572fdcf3d888ab8f
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-17 12:55:24 -0500

    setting up basic routes. cleaning up hrefs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,24 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import Home from './pages/Home';
+import ProductPage from './pages/ProductPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/product/:id' element={<ProductPage/>} />
+          </Routes>
         </Container>
       </main>
       <Footer />
+      </BrowserRouter>
     </>
   );
 };

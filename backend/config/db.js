70ddfb1097bc5940eee10d873dd6f07a1ff601dc
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-19 10:42:03 -0500

    mongoose connection. colors for terminal

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  mongoose.set('strictQuery', false);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
